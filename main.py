e801a17b49158491e9875cede615518ad12523e6
BolajiOlajide <bolaji.olajide@andela.com>
2019-02-17 14:51:42 +0100

    some more pybald

--- main.py
+++ main.py
@@ -12,12 +12,26 @@ pybald.configure(config_object=config)
 
 def map(urls):
     urls.connect('home', r'/', controller='home')
+    urls.connect('show_blog', r'/blog', controller='home',
+                 action='showBlog')
+    urls.connect('show_post', r'/blog/posts/{id}', controller='post',
+                 action='show', conditions={"method": ["GET", "HEAD"]})
+
+
+class PostController(Controller):
+    @action
+    def show(self, req):
+        return "Posting a new blog post"
 
 
 class HomeController(Controller):
     @action
     def index(self, req):
         return "Hello!"
+
+    @action
+    def showBlog(self, req):
+        return "Welcome to my blog!"
 
 
 app = Router(routes=map, controllers=[HomeController])
